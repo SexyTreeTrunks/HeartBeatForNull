739facdc5d950aafc1eb0610905c4652139c239b
HeaIn <rosum999@naver.com>
2017-08-15 19:22:21 +0900

    added searchpath

--- app/src/main/java/com/sookmyung/heartbeatfornull/searchpath/PlacePredictionActivity.java
+++ app/src/main/java/com/sookmyung/heartbeatfornull/searchpath/PlacePredictionActivity.java
@@ -10,15 +10,16 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.sookmyung.heartbeatfornull.R;
 
-public class PlacePredictionActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener{
-//public class PlacePredictionActivity extends FragmentActivity {
+public class PlacePredictionActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient mGoogleApiClient;
     int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE2 = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +27,38 @@ public class PlacePredictionActivity extends FragmentActivity implements GoogleA
         setContentView(R.layout.activity_placeprediction);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, this).build();
+                .enableAutoManage(this, this).build();//api 추가 부분
         mGoogleApiClient.connect();
 
         try {
+            //검색어 자동완성 오버레이
             Intent search_intent =
                     new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
                             .build(this);
-            Log.i("***","this");
-
             startActivityForResult(search_intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+
+
         } catch (GooglePlayServicesRepairableException e) {
             // TODO: Handle the error.
-            Log.e("***e","er1", e);
+            Log.e("***e", "er1", e);
         } catch (GooglePlayServicesNotAvailableException e) {
             // TODO: Handle the error.
-            Log.e("***e","er2", e);
+            Log.e("***e", "er2", e);
         }
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                //Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i("***", "Place: " );//+ place.getName());
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i("***", "Place: " + place.getName());
+                //이전 액티비티 호출
+                Intent return_intent = getIntent();
+                return_intent.putExtra("place", place.getName());
+                setResult(RESULT_OK, return_intent);
+                finish();
+
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO: Handle the error.
@@ -61,6 +68,27 @@ public class PlacePredictionActivity extends FragmentActivity implements GoogleA
                 // The user canceled operation.
                 Log.i("***er", "er");
 
+            }
+        }
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE2) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i("***", "Place: " + place.getName());
+                //이전 액티비티 호출
+                Intent return_intent = getIntent();
+                return_intent.putExtra("place", place.getName());
+                setResult(RESULT_OK, return_intent);
+                finish();
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i("***", status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled operation.
+                Log.i("***er", "er");
+                PlacePredictionActivity.this.finish();
             }
         }
     }
@@ -73,8 +101,7 @@ public class PlacePredictionActivity extends FragmentActivity implements GoogleA
 
         // ...
 
-        Log.i("***er","er");
+        Log.i("***er", "er");
         // TODO: 2017-08-13 https://developers.google.com/android/guides/api-client?hl=ko#HandlingFailures
     }
-
 }
--- app/src/main/java/com/sookmyung/heartbeatfornull/searchpath/SelectLocationActivity.java
+++ app/src/main/java/com/sookmyung/heartbeatfornull/searchpath/SelectLocationActivity.java
@@ -1,11 +1,11 @@
 package com.sookmyung.heartbeatfornull.searchpath;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,95 +13,131 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.sookmyung.heartbeatfornull.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by jsm95 on 2017-07-24.
  */
 
-public class SelectLocationActivity extends Activity{
+public class SelectLocationActivity extends AppCompatActivity implements GoogleMap.OnMarkerDragListener {
     double slat;
     double slon;
     double elat;
     double elon;
+    LatLng ROUTE;
+
+    private GoogleMap mMap;
+    private HttpRequestAsyncTask httpRequestAsyncTask;
+    private List<List<HashMap<String, String>>> wayPointsList ;
+
+    String strStartloc = null;
+    String strEndloc = null;
+
+    String testStr = "";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectlocations);
 
-        Button button_select =(Button)findViewById(R.id.button_select); //출발지 선택 버튼
-        Button button_endlocation = (Button)findViewById(R.id.button_endlocation); //도착지 선택 버튼
-
-        final TextView tv = (TextView)findViewById(R.id.textView); //출발지 위도 경도 txt
-        final TextView endtv = (TextView)findViewById(R.id.textView_endloc); //도착지 위도 경도 txt
+        Button button_endlocation = (Button)findViewById(R.id.button_endlocation); //goto 3D
 
         final EditText editText_location = (EditText)findViewById(R.id.editText_location); //출발지 입력박스 문구
         final EditText editText_endlocation = (EditText)findViewById(R.id.editText_endlocation); //도착지 입력박스 문구
 
-        final Geocoder geocoder = new Geocoder(this);
-
         editText_location.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if (editText_location.length() >= 0) {
-                    editText_location.setText(null);
-                    Intent intent = new Intent(SelectLocationActivity.this, PlacePredictionActivity.class);
-                    startActivity(intent);
-                    Log.i("***intent",editText_location.toString());
-                }
-
-                Log.i("***","intent");
+                Intent intent = new Intent(SelectLocationActivity.this, PlacePredictionActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
 
-        button_select.setOnClickListener(new View.OnClickListener() {
+        editText_endlocation.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                List<Address> list = null;
-                String str_location = editText_location.getText().toString();
-                try{
-                    list = geocoder.getFromLocationName(str_location, 10);
-                } catch (IOException e){
-                    e.printStackTrace();
+                Intent intent = new Intent(SelectLocationActivity.this, PlacePredictionActivity.class);
+                startActivityForResult(intent, 2);
+            }
+        });
 
-                    Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러 발생");
-                }
+        /*button_endlocation.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectLocationActivity.this, ResultMapFromSearch.class);
+                startActivityForResult(intent, 2);
+            }
+        });*/
+    }
 
-                if(list != null) {
-                    if (list.size() == 0) {
-                       Toast.makeText(SelectLocationActivity.this,"해당 주소 없음", Toast.LENGTH_LONG).show();
-                    } else {
-                        Address addr = list.get(0);
-                        double lat = addr.getLatitude();
-                        double lon = addr.getLongitude();
+    @Override
+    protected void onActivityResult( int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
 
-                        String str_lat = String.format("%.6f",lat);
-                        slat = Double.parseDouble(str_lat);
+        final EditText editText_location = (EditText)findViewById(R.id.editText_location);
+        final EditText editText_endlocation = (EditText)findViewById(R.id.editText_endlocation);
 
-                        String str_lon = String.format("%.6f",lon);
-                        slon = Double.parseDouble(str_lon);
+        List<Address> list = null;
 
-                        // TODO: 2017-08-01 append settext로 바꾸기
-                        tv.append(Double.toString(slat) +", " +Double.toString(slon));
+        String result_txt = null;
+
+        final Geocoder geocoder = new Geocoder(SelectLocationActivity.this);
+
+        //위도 경도 표시용 txt
+        final TextView tv = (TextView)findViewById(R.id.textView);
+        final TextView endtv = (TextView)findViewById(R.id.textView_endloc);
+
+        if(resultCode==RESULT_OK) // 액티비티가 정상적으로 종료되었을 경우
+        {
+            if(requestCode==1)
+                {
+                    list = null;
+                    result_txt = data.getStringExtra("place");
+                    editText_location.setText(result_txt);
+                    try{
+                        list = geocoder.getFromLocationName(result_txt, 10);
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                    if(list != null) {
+                        if (list.size() == 0) {
+                            Toast.makeText(SelectLocationActivity.this,"해당 주소 없음", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Address addr = list.get(0);
+                            double lat = addr.getLatitude();
+                            double lon = addr.getLongitude();
+
+                            slat = Double.parseDouble(String.format("%.6f",lat));
+                            slon = Double.parseDouble(String.format("%.6f",lon));
+
+                            tv.setText(Double.toString(slat) +", " +Double.toString(slon));
                     }
                 }
-            }
-        });
 
-        button_endlocation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                List<Address> list = null;
-                String str_location = editText_endlocation.getText().toString();
+            }
+            else if(requestCode==2)
+            {
+                list = null;
+                result_txt = data.getStringExtra("place");
+                editText_endlocation.setText(result_txt);
                 try{
-                    list = geocoder.getFromLocationName(str_location, 10);
+                    list = geocoder.getFromLocationName(result_txt, 10);
                 } catch (IOException e){
                     e.printStackTrace();
-
-                    Log.e("Endloc failed", "입출력 오류 - 서버에서 주소변환시 에러 발생");
                 }
-
                 if(list != null) {
                     if (list.size() == 0) {
                         Toast.makeText(SelectLocationActivity.this,"해당 주소 없음", Toast.LENGTH_LONG).show();
@@ -116,17 +152,117 @@ public class SelectLocationActivity extends Activity{
                         String str_lon = String.format("%.6f",lon);
                         elon = Double.parseDouble(str_lon);
 
-                        endtv.append(Double.toString(elat) +", " +Double.toString(elon));
+                        endtv.setText(Double.toString(elat) +", " +Double.toString(elon));
 
-                        Intent endloc_intent = new Intent(SelectLocationActivity.this, ResultMapFromSearch.class); //intent 바꾸기
-                        endloc_intent.putExtra("startlat",slat);
-                        endloc_intent.putExtra("startlon",slon);
-                        endloc_intent.putExtra("endlat", elat);
-                        endloc_intent.putExtra("endlon", elon);
-                        startActivity(endloc_intent);
+                    }
+                    try {
+
+                        httpRequestAsyncTask = new HttpRequestAsyncTask(){
+                            //doinbackground함수가 다 실행되고 자동으로 실행되는 함수임!
+                            @Override
+                            protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+                                //경로정보 전역변수에 저장
+                                wayPointsList = result;
+                                //2d지도에 폴리라인 그리깅
+                                set2dMapView();
+                            }
+                        };
+                        httpRequestAsyncTask.execute(slat, slon, elat, elon);
+                    }
+                    catch(Exception e){
+                        Log.e("***AsynchTask오류","mapactivity85");
                     }
                 }
             }
+        }
+    }
+
+    private void set2dMapView() {
+        //2D map setting
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.route_map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                ROUTE = new LatLng(elat, elon);
+                googleMap.setOnMarkerDragListener(SelectLocationActivity.this);
+                // Creates a draggable marker. Long press to drag.
+                googleMap.moveCamera((CameraUpdateFactory.newLatLng(ROUTE)));
+
+                mMap = googleMap;
+
+                drawMarkerOn2dMap();
+                drawPathLineOn2dMap();
+            }
         });
+    }
+
+    private void drawMarkerOn2dMap() {
+        //시작, 출발점 마커를 그려용
+        MarkerOptions markerOptions = new MarkerOptions();
+        LatLng startLatLng = new LatLng(slat,slon);
+        markerOptions.position(startLatLng);
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        mMap.addMarker(markerOptions);
+
+        markerOptions = new MarkerOptions();
+        LatLng endLatLng = new LatLng(elat, elon);
+        markerOptions.position(endLatLng);
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        mMap.addMarker(markerOptions);
+    }
+
+    private void drawPathLineOn2dMap() {
+        ArrayList points = null;
+        PolylineOptions lineOptions = null;
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        for (int i = 0; i < wayPointsList.size(); i++) {
+            points = new ArrayList();
+            lineOptions = new PolylineOptions();
+
+            List<HashMap<String, String>> path = wayPointsList.get(i);
+
+            for (int j = 0; j < path.size(); j++) {
+                HashMap<String, String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                points.add(position);
+            }
+
+            lineOptions.addAll(points);
+            lineOptions.width(12);
+            lineOptions.color(Color.RED);
+            lineOptions.geodesic(true);
+        }
+
+        // Drawing polyline in the Google Map for the i-th route
+        mMap.addPolyline(lineOptions);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //outState.putParcelable(MARKER_POSITION_KEY, mMarker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        //mStreetViewPanorama.setPosition(marker.getPosition(), 150);
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
     }
 }
